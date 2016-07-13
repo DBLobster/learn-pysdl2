b2bbeb129f6e225c6f247828e514145e9ab23429
DBLobster <db.lobster@gmail.com>
2016-07-13 22:04:52 +0800

    add two examples.

---
+++ _static/src/hello.py
@@ -0,0 +1,23 @@
+import sdl2.ext
+
+RESOURCES = sdl2.ext.Resources(__file__, 'res')
+
+sdl2.ext.init()
+
+window = sdl2.ext.Window('Hello World!', size=(320, 240))
+window.show()
+
+factory = sdl2.ext.SpriteFactory(sdl2.ext.SOFTWARE)
+
+#renderer = sdl2.ext.Renderer(window)
+#factory = sdl2.ext.SpriteFactory(sdl2.ext.TEXTURE, renderer=renderer)
+
+sprite = factory.from_image(RESOURCES.get_path('hello.png'))
+
+spriterenderer = factory.create_sprite_render_system(window)
+spriterenderer.render(sprite)
+
+processor = sdl2.ext.TestEventProcessor()
+processor.run(window)
+
+sdl2.ext.quit()
---
+++ _static/src/hello_ttf.py
@@ -0,0 +1,36 @@
+import sdl2.ext
+
+RESOURCES = sdl2.ext.Resources(__file__, 'res')
+
+#TTF = 'C:\\Windows\\Fonts\\simkai.ttf' # Windows
+#TTF = '/usr/share/fonts/truetype/unifont/unifont.ttf' # Debian
+TTF = RESOURCES.get_path('unifont-8.0.01.ttf') # 自备字体
+
+white = sdl2.ext.Color(255, 255, 255)
+black = sdl2.ext.Color(0, 0, 0)
+
+sdl2.ext.init()
+
+fm = sdl2.ext.FontManager(TTF, color=black, bg_color=white)
+
+window = sdl2.ext.Window('Hello World!', size=(320, 240))
+window.show()
+
+factory = sdl2.ext.SpriteFactory(sdl2.ext.SOFTWARE)
+
+#renderer = sdl2.ext.Renderer(window)
+#factory = sdl2.ext.SpriteFactory(sdl2.ext.TEXTURE, renderer=renderer)
+
+sprite = factory.from_image(RESOURCES.get_path('hello.png'))
+sprite_eng = factory.from_text('Hello World!', fontmanager=fm)
+sprite_chs = factory.from_text('你好, 世界!', fontmanager=fm)
+
+spriterenderer = factory.create_sprite_render_system(window)
+spriterenderer.render(sprite)
+spriterenderer.render(sprite_eng, 0, 16*4)
+spriterenderer.render(sprite_chs, 0, 16*5)
+
+processor = sdl2.ext.TestEventProcessor()
+processor.run(window)
+
+sdl2.ext.quit()
